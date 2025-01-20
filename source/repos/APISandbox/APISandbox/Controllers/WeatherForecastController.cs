d0886e2e2194f8f0eb5453365fa5767e6ce82d72
JoseMontero09 <jopamon4@gmail.com>
2025-01-20 15:43:27 -0600

    Inicio de la API en el lab7

---
+++ source/repos/APISandbox/APISandbox/Controllers/WeatherForecastController.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace APISandbox.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [Route("[action]/{date}")]
+        [HttpGet]
+        public WeatherForecast GetByDate(DateOnly date)
+        {
+            var rng = new Random();
+            return new WeatherForecast
+            {
+                Date = date,
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            };
+        }
+
+
+        [Route("[action]")]
+        [HttpPost]
+        public IEnumerable <WeatherForecast> Post(WeatherForecast weatherForecast)
+        {
+            List<WeatherForecast> weatherForecasts = Get().ToList();
+            weatherForecasts.Add(weatherForecast);
+            return weatherForecasts;
+            
+        }
+
+        [Route("[action]")]
+        [HttpPatch]
+        public WeatherForecast Patch(WeatherForecast weatherForecast)
+        {
+            return new WeatherForecast
+            {
+                Date = weatherForecast.Date,
+                TemperatureC = weatherForecast.TemperatureC,
+                Summary = weatherForecast.Summary
+            };
+        }
+
+        [Route("[action]")]
+        [HttpPut]
+        public WeatherForecast Put(WeatherForecast weatherForecast)
+        {
+            return new WeatherForecast
+            {
+                Date = weatherForecast.Date,
+                TemperatureC = weatherForecast.TemperatureC,
+                Summary = weatherForecast.Summary
+            };
+        }
+
+    }
+}
